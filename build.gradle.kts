35616c416f203e9b9c58b0eb4f24c03f973d7d16
TheMather1 <the.mather1@gmail.com>
2022-06-20 12:10:21 +0200

    Add release workflow.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,77 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
+    `maven-publish`
+    `java-library`
 }
 
-group = "pathfinder"
-version = "1.0-SNAPSHOT"
+group = "no.mather.ttrpg"
+version = System.getenv("RELEASE_VERSION")
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/TheMather1/${rootProject.name}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register("gpr", MavenPublication::class) {
+            artifactId = "dice-syntax"
+            from(components["java"])
+            pom {
+                name.set("dice-syntax")
+                description.set("Advanced dice syntax and parsing.")
+                url.set("https://github.com/TheMather1/${rootProject.name}")
+                inceptionYear.set("2022")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Mathias Sand Jahren")
+                        email.set("the.mather1@gmail.com")
+                        url.set("https://github.com/TheMather1")
+                        timezone.set("CET")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/TheMather1/${rootProject.name}.git")
+                    developerConnection.set("scm:git:ssh://github.com/TheMather1/${rootProject.name}.git")
+                    url.set("https://github.com/TheMather1/${rootProject.name}/tree/main")
+                }
+            }
+        }
+    }
+}
+
+tasks{
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
+    }
 }
